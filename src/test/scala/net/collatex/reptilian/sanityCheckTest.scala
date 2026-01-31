e089f928dc6510f78fbf3a2146836deea27f03c7
David J Birnbaum <djbpitt@gmail.com>
2026-01-31 16:21:48 -0500

    Create test for sanity-check logging with all combinations of missing and duplicate tokens

--- src/test/scala/net/collatex/reptilian/sanityCheckTest.scala
+++ src/test/scala/net/collatex/reptilian/sanityCheckTest.scala
@@ -39,13 +39,83 @@ class sanityCheckTest extends AnyFunSuite:
   }
 
   test("Correctly logs missing (no duplicate) tokens in alignment") {
-    assert(true)
+    val gTa: Vector[TokenEnum] = Vector(
+      Token("a", "a", 0, 0, Map()),
+      TokenSep("s", "s", 0, 1),
+      Token("a", "a", 1, 2, Map()),
+      Token("b", "b", 1, 3, Map()) // Token 3 not part of alignment
+    )
+    val root = AlignmentRibbon(
+      ListBuffer[_root_.net.collatex.reptilian.AlignmentUnit](
+        AlignmentPoint(
+          Map(0 -> TokenRange(0, 1, gTa), 1 -> TokenRange(2, 3, gTa)),
+          Set(Map(0 -> TokenRange(0, 1, gTa), 1 -> TokenRange(2, 3, gTa)))
+        )
+      )
+    )
+    sanityLogging(root, gTa)
+    val lastEventMessage = listAppender.list.getLast.getMessage
+    assert(lastEventMessage == "Missing tokens: Set(3)")
   }
 
   test("Correctly logs duplicate (no missing) tokens in alignment") {
-    assert(true)
+    val gTa: Vector[TokenEnum] = Vector(
+      Token("a", "a", 0, 0, Map()),
+      Token("ab", "ab", 0, 1, Map()),
+      Token("b", "b", 0, 2, Map()),
+      TokenSep("s", "s", 0, 3),
+      Token("a", "a", 1, 4, Map()),
+      Token("ab", "ab", 1, 5, Map()),
+      Token("ab", "ab", 1, 6, Map()),
+      Token("b", "b", 1, 7, Map())
+    )
+    val root = AlignmentRibbon(
+      ListBuffer[_root_.net.collatex.reptilian.AlignmentUnit](
+        AlignmentPoint(
+          Map(0 -> TokenRange(0, 2, gTa), 1 -> TokenRange(4, 6, gTa)),
+          Set(Map(0 -> TokenRange(0, 2, gTa), 1 -> TokenRange(4, 6, gTa)))
+        ),
+        AlignmentPoint(
+          Map(0 -> TokenRange(1, 3, gTa), 1 -> TokenRange(6, 8, gTa)),
+          Set(Map(0 -> TokenRange(1, 3, gTa), 1 -> TokenRange(6, 8, gTa)))
+        )
+      )
+    )
+    sanityLogging(root, gTa)
+    val lastEventMessage = listAppender.list.getLast.getMessage
+    assert(lastEventMessage == "Duplicate tokens: List(1)")
   }
 
   test("Correctly logs both duplicate and missing tokens in alignment") {
-    assert(true)
+    val gTa: Vector[TokenEnum] = Vector(
+      Token("a", "a", 0, 0, Map()),
+      Token("ab", "ab", 0, 1, Map()),
+      Token("b", "b", 0, 2, Map()),
+      TokenSep("s", "s", 0, 3),
+      Token("a", "a", 1, 4, Map()),
+      Token("ab", "ab", 1, 5, Map()),
+      Token("ab", "ab", 1, 6, Map()),
+      Token("b", "b", 1, 7, Map()),
+      Token("c", "c", 1, 8, Map())
+    )
+    val root = AlignmentRibbon(
+      ListBuffer[_root_.net.collatex.reptilian.AlignmentUnit](
+        AlignmentPoint(
+          Map(0 -> TokenRange(0, 2, gTa), 1 -> TokenRange(4, 6, gTa)),
+          Set(Map(0 -> TokenRange(0, 2, gTa), 1 -> TokenRange(4, 6, gTa)))
+        ),
+        AlignmentPoint(
+          Map(0 -> TokenRange(1, 3, gTa), 1 -> TokenRange(6, 8, gTa)),
+          Set(Map(0 -> TokenRange(1, 3, gTa), 1 -> TokenRange(6, 8, gTa)))
+        )
+      )
+    )
+    sanityLogging(root, gTa)
+    // We log duplicates before missings
+    // Removes and returns; parentheses required by Java (not Scala)
+    System.err.println(s"list: ${listAppender.list}")
+    val lastEventMessage = listAppender.list.removeLast().getMessage
+    assert(lastEventMessage == "Missing tokens: Set(8)")
+    val penultimateEventMessage = listAppender.list.removeLast().getMessage
+    assert(penultimateEventMessage == "Duplicate tokens: List(1)")
   }
