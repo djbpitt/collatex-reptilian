b3aea0510dcbf691093b68ed4afc02e44c0e1bd5
David J Birnbaum <djbpitt@gmail.com>
2026-02-24 15:07:06 -0500

    Complete first endpoint; no server yet

--- src/main/scala/net/collatex/util/helloTapir.scala
+++ src/main/scala/net/collatex/util/helloTapir.scala
@@ -1,12 +1,15 @@
 package net.collatex.util
 // https://tapir.softwaremill.com/en/latest/tutorials/01_hello_world.html
 
+import sttp.shared
 import sttp.tapir.*
+import sttp.tapir.server.ServerEndpoint
 
 @main def helloWorldTapir(): Unit =
-  val helloWorldEndpoint = endpoint
-    .get
+  val helloWorldEndpoint: ServerEndpoint[Any, shared.Identity] = endpoint.get
     .in("hello" / "world")
     .in(query[String]("name"))
+    .out(stringBody)
+    .handleSuccess(name => s"Hello, $name!")
 
   println(helloWorldEndpoint.show)
